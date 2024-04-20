bab56192542e7acfc43a85b98cd73b9bce7d3758
mediocrest-webdev <mediocrestdev@gmail.com>
2024-04-20 20:24:17 +0800

    added contact and our team sections

---
+++ src/data/project.json.ts
@@ -0,0 +1,33 @@
+export const projects = [
+    {
+        link:"/projects/sweden",
+        src: import('../images/project-image-1.webp'),
+        alt: "house-2",
+        date: "2012-2015",
+        title: "Sweden"
+    },
+    {
+        link:"/projects/france",
+        src: import('../images/project-image-2.webp'),
+        alt: "house-3",
+        date: "2017-2018",
+        title: "Paris, France"
+    },
+    {
+        link:"/projects/london",
+        src: import('../images/project-image-3.webp'),
+        alt: "house-4",
+        date: "2018-2021",
+        title: "London, UK"
+    },
+    {
+        link:"/projects/philippines",
+        src: import('../images/project-image-4.webp'),
+        alt: "house-5",
+        date: "2022-2023",
+        title: "Manila, Philippines"
+    },
+]
+
+
+
---
+++ src/data/team.json.ts
@@ -0,0 +1,26 @@
+export const teams = [
+    {
+        src: import('../images/team-member (1).webp'),
+        alt: "white man in coat, with glasses",
+        name: "John Standford",
+        position: "Interior Designer"
+    },
+    {
+        src: import('../images/team-member (2).webp'),
+        alt: "a man in longsleeves polo with hands clamped together and on the table",
+        name: "Louie Reyes",
+        position: "Architect"
+    },
+    {
+        src: import('../images/team-member (3).webp'),
+        alt: "a man smiling with his hands on his back",
+        name: "Mark Anthony",
+        position: "Interior Designer"
+    },
+    {
+        src: import('../images/team-member (4).webp'),
+        alt: "a professional woman smiling on the camera",
+        name: "Anne Lim",
+        position: "Civil Engineer"
+    },
+]
